65e7b4c1a189b2435dc19ca4968e08837a05921f
Emik <emik.lakunishok@gmail.com>
2022-12-22 13:46:33 +0100

    Update code style

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,6 +1,13 @@
+#region Emik.MPL
+
 // <copyright file="GlobalUsings.cs" company="Emik">
 // Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
+
+#endregion
+
+#region
+
 global using Celeste.Mod;
 global using FMOD;
 global using FMOD.Studio;
@@ -11,10 +18,11 @@ global using static Celeste.Mod.Everest.Loader;
 global using static Celeste.TextMenu;
 global using Audio = Celeste.Audio;
 global using AudioState = On.Celeste.AudioState;
-global using Debug = System.Diagnostics.Debug;
 global using Dialog = Celeste.Dialog;
 global using EventInstance = FMOD.Studio.EventInstance;
 global using Level = Celeste.Level;
 global using OnAudio = On.Celeste.Audio;
 global using TextMenu = Celeste.TextMenu;
 global using ZipFile = Ionic.Zip.ZipFile;
+
+#endregion
